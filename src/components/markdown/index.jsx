30beb6072a2cee40b6efab313f3be71d2ce441cd
Nikola Borisic <borisic.nikola@yahoo.com>
2022-09-26 16:47:02 +0200

    Add 6th news

--- src/components/markdown/index.jsx
+++ src/components/markdown/index.jsx
@@ -4,68 +4,68 @@ import rehypeRaw from 'rehype-raw';
 
 import styles from './index.module.scss';
 
-const Markdown = ({
-  content,
-  // ...htmlProps
-}) => {
+const Markdown = ({ content, ...htmlProps }) => {
   return (
     <ReactMarkdown
       remarkPlugins={[remarkGfm]}
       rehypePlugins={[rehypeRaw]}
-      components={
-        {
-          // a: ({ ...props }) => (
-          //   <TextLink {...htmlProps} href={props.href} textTagName="span">
-          //     <Body tagName="span" lineHeight="long" size={2}>
-          //       {props.children}
-          //     </Body>
-          //   </TextLink>
-          // ),
-          // p: ({ ...props }) => (
-          //   <Body
-          //     {...htmlProps}
-          //     lineHeight="long"
-          //     size={2}
-          //     className={styles.paragraph}
-          //   >
-          //     {props.children}
-          //   </Body>
-          // ),
-          // h5: ({ ...props }) => (
-          //   <Heading size={1} {...htmlProps} className={styles.heading}>
-          //     {props.children}
-          //   </Heading>
-          // ),
-          // h4: ({ ...props }) => (
-          //   <Heading size={2} {...htmlProps} className={styles.heading}>
-          //     {props.children}
-          //   </Heading>
-          // ),
-          // h3: ({ ...props }) => (
-          //   <Heading size={3} {...htmlProps} className={styles.heading}>
-          //     {props.children}
-          //   </Heading>
-          // ),
-          // h2: ({ ...props }) => (
-          //   <Heading size={4} {...htmlProps} className={styles.heading}>
-          //     {props.children}
-          //   </Heading>
-          // ),
-          // h1: ({ ...props }) => (
-          //   <Heading size={5} {...htmlProps} className={styles.heading}>
-          //     {props.children}
-          //   </Heading>
-          // ),
-          // hr: () => (
-          //   <Divider
-          //     {...htmlProps}
-          //     className={classnames(styles.divider, {
-          //       [styles.dividerWithSidebar]: hasSidebar,
-          //     })}
-          //   />
-          // ),
-        }
-      }
+      components={{
+        a: ({ ...props }) => (
+          <a
+            {...htmlProps}
+            href={props.href}
+            target="_blank"
+            rel="noopener noreferrer"
+            textTagName="span"
+            className={styles.link}
+          >
+            {props.children}
+          </a>
+        ),
+        // p: ({ ...props }) => (
+        //   <Body
+        //     {...htmlProps}
+        //     lineHeight="long"
+        //     size={2}
+        //     className={styles.paragraph}
+        //   >
+        //     {props.children}
+        //   </Body>
+        // ),
+        // h5: ({ ...props }) => (
+        //   <Heading size={1} {...htmlProps} className={styles.heading}>
+        //     {props.children}
+        //   </Heading>
+        // ),
+        // h4: ({ ...props }) => (
+        //   <Heading size={2} {...htmlProps} className={styles.heading}>
+        //     {props.children}
+        //   </Heading>
+        // ),
+        // h3: ({ ...props }) => (
+        //   <Heading size={3} {...htmlProps} className={styles.heading}>
+        //     {props.children}
+        //   </Heading>
+        // ),
+        // h2: ({ ...props }) => (
+        //   <Heading size={4} {...htmlProps} className={styles.heading}>
+        //     {props.children}
+        //   </Heading>
+        // ),
+        // h1: ({ ...props }) => (
+        //   <Heading size={5} {...htmlProps} className={styles.heading}>
+        //     {props.children}
+        //   </Heading>
+        // ),
+        // hr: () => (
+        //   <Divider
+        //     {...htmlProps}
+        //     className={classnames(styles.divider, {
+        //       [styles.dividerWithSidebar]: hasSidebar,
+        //     })}
+        //   />
+        // ),
+      }}
     >
       {content}
     </ReactMarkdown>
--- src/utils/imageMap.js
+++ src/utils/imageMap.js
@@ -3,6 +3,7 @@ import imageTwo from '../assets/images/news/2.jpg';
 import imageTree from '../assets/images/news/3.jpg';
 import imageFour from '../assets/images/news/4.jpg';
 import imageFive from '../assets/images/news/5.jpg';
+import imageSix from '../assets/images/news/6.jpg';
 
 const imageMap = {
   1: imageOne,
@@ -10,6 +11,7 @@ const imageMap = {
   3: imageTree,
   4: imageFour,
   5: imageFive,
+  6: imageSix,
 };
 
 export default imageMap;
